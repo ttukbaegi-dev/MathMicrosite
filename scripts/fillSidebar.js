f1a709c711d4f380e678fd1d4c23894bc16ecec2
Cookie <cookiesncrema31@gmail.com>
2024-02-25 15:06:10 -0500

    using network MathJax resource

--- scripts/fillSidebar.js
+++ scripts/fillSidebar.js
@@ -1,7 +1,30 @@
+function urlify(str) {
+    return str
+        .replace(' ', '_')
+        .replace("'", '%27');
+}
+
 let headings = document.getElementsByTagName("h1");
 let sidebar = document.getElementById("sidebar");
 for (const h of headings) {
     let hText = h.textContent;
     sidebar.innerHTML
-        += '<a href="#' + hText.replace(" ", "_") + '">' + hText + "</a>";
+        += '<a href="#' + urlify(hText) + '">' + hText + "</a>";
 }
+
+let page_select = 
+       '<div id="page-nav">'
+     + '<label id="page-select-label" for="page-select">Choose a page:</label>'
+     + '<select name="page" id="page-select">';
+let pages = [
+    "arithmetic",
+    "plotting",
+    "systems of linear equations"
+];
+for (const p of pages) {
+    page_select
+        += '<option value="' + urlify(p) + '">' + p + '</option>';
+}
+page_select
+    += '</select></div>';
+sidebar.innerHTML += page_select;
